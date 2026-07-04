243c53096e67fa88045479ce6b50431bf4608147
David Masegosa <davidmasegosa@gmail.com>
2026-07-04 12:32:59 +0200

    feat: get books, get books by genre, sort books by year , get book by id

--- index.js
+++ index.js
@@ -12,3 +12,30 @@ app.use(express.json());
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+app.get('/api/books/:id', (req, res) => {
+    const book = books.find(book => book.id === parseInt(req.params.id));
+
+    if(book) {
+      res.json(book);
+    } else {
+      res.status(404).json({ code: 'not_found' });
+    }
+});
+
+app.get('/api/books', (req, res) => {
+
+  let filteredBooks = [];
+
+  if(req.query.genre) {
+    filteredBooks = books.filter(book => book.genre === req.query.genre);
+  } else {
+    filteredBooks = books;
+  }
+
+  if(req.query.sort && req.query.sort === 'year') {
+    filteredBooks = filteredBooks.sort((a, b) => a.year - b.year);
+  }
+
+  res.json(filteredBooks);
+});
